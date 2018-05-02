aec30eb477da57531d2df4f9400258e064e45da5
03247892206 <03247892206@JIPA-LABIN02-04.ifro.local>
2018-05-02 10:02:28 -0400

    Criando Calculadora

--- 3a_Nathalya/src/br/edu/ifro/MenuPrincipalController.java
+++ 3a_Nathalya/src/br/edu/ifro/MenuPrincipalController.java
@@ -10,7 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -19,12 +21,19 @@ import javafx.scene.control.Label;
 public class MenuPrincipalController implements Initializable {
     
     @FXML
-    private Label label;
+    private TextField txtNumero1, txtNumero2, txtResultado;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Button btnSoma;
+    
+    @FXML
+    
+    private void Soma(ActionEvent event) {
+        Double num1 = Double.parseDouble(txtNumero1.getText());
+        Double num2 = Double.parseDouble(txtNumero2.getText());
+        Double resultado = num1 + num2;
+        
+        txtResultado.setText(resultado.toString());
     }
     
     @Override
